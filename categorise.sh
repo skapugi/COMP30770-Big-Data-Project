480fa02d5d5a6b753af1f7c66e6f1c53b12123be
Sophie Kapugi <sophiekapugi@aenead.net>
2026-02-19 19:05:37 +0000

    Basic searching through file

--- categorise.sh
+++ categorise.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "Hello"
+# Data file to sort through
+DATAFILE="GitHubJanuary.json"
+
+# Ensure dependencies are present
+require() {
+  if ! command -v jq &> /dev/null; then
+    echo "jq command is not present. Please install command or run in Colab"
+    exit 1
+  fi
+}
+require
+
+while IFS= read -r line; do
+    if [ -n "$line" ]; then
+        id="$(jq -r '.id' <<<"$line")"
+        type="$(jq -r '.type' <<<"$line")"
+
+        echo "id: $id"
+        echo "type: $type"
+
+        echo "---"
+    fi
+done < "$DATAFILE"
